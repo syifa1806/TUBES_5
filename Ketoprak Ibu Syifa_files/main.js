1ada43aaf4a4eb178e7842d0509a528037bde706
ayundanisabilah <yuesa233@gmail.com>
2024-12-26 13:00:36 +0700

    Add files via upload

---
+++ Ketoprak Ibu Syifa_files/main.js
@@ -0,0 +1,9 @@
+
+const menuIcon = document.getElementById("menu-icon");
+const navbarNav = document.getElementById("navbar-nav"); 
+
+
+menuIcon.addEventListener("click", () => {
+    
+    navbarNav.classList.toggle("show");
+});
