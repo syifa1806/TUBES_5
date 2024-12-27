f56ad577c457f5d33c7a6a526e5e759575a726c3
syifa1806 <syifasyauqiyah24@student.polindra.ac.id>
2024-12-27 19:25:13 +0700

    Delete Ketoprak Ibu Syifa_files directory

--- Ketoprak Ibu Syifa_files/main.js
+++
@@ -1,9 +0,0 @@
-
-const menuIcon = document.getElementById("menu-icon");
-const navbarNav = document.getElementById("navbar-nav"); 
-
-
-menuIcon.addEventListener("click", () => {
-    
-    navbarNav.classList.toggle("show");
-});
